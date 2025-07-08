00875d1aa612bf0cc3eaed76f0336e20dcb21c2d
Mausam Thapa <aasulink123@gmail.com>
2025-07-08 13:45:40 +0545

    Lab1

---
+++ CoreJava/Lab1/MathOperations.java
@@ -0,0 +1,43 @@
+package CoreJava.Lab1;
+
+import java.util.Scanner;
+
+class MathOperations {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double number1, number2;
+
+        System.out.print("Enter the first number : ");
+        number1 = sc.nextDouble();
+
+        System.out.print("Enter the second number : ");
+        number2 = sc.nextDouble();
+
+        double sum = number1 + number2;
+
+        double product = number1 * number2;
+
+        double quotient, remainder;
+        if (number2 != 0) {
+            quotient = number1 / number2;
+            remainder = number1 % number2;
+        } else {
+            quotient = Double.NaN;
+            remainder = Double.NaN;
+            System.out.println("Cannot be divisible by zero.");
+        }
+
+        double average = sum / 2;
+
+        System.out.println("\nResults:");
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+        System.out.println("Average: " + average);
+
+        sc.close();
+    }
+}
