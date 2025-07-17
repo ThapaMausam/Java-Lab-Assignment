bf9d2e029cc37f22fc6ff8df747f488595174ba9
Mausam Thapa <mausamwork@gmail.com>
2025-07-17 10:14:17 +0545

    Complete Notepad App

--- AdvancedJava/Lab5/Notepad.java
+++ AdvancedJava/Lab5/Notepad.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 public class Notepad {
     public static void main(String[] args) {
@@ -15,7 +16,14 @@ public class Notepad {
 
         // Create textArea
         JTextArea textArea = new JTextArea();
+
+        textArea.setLineWrap(true); // Enable line wrapping
+        textArea.setWrapStyleWord(true); // Wrap at word boundaries
+
         JScrollPane scrollPane = new JScrollPane(textArea);
+
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); // Always show vertical scrollbar
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // Always hide horizontal
 
         // Create JPanel
         JPanel buttonPanel = new JPanel();
@@ -25,6 +33,9 @@ public class Notepad {
         JButton openButton = new JButton("Open");
         JButton saveButton = new JButton("Save");
         JButton exitButton = new JButton("Exit");
+
+        // Create filechooser
+        JFileChooser fileChooser = new JFileChooser();
 
         // Add ActionListener to buttons
         // newButton
@@ -36,19 +47,49 @@ public class Notepad {
             }
         });
 
+        // openButton
+        openButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int choice = fileChooser.showOpenDialog(frame);
+                if (choice == JFileChooser.APPROVE_OPTION) {
+                    File currentFile = fileChooser.getSelectedFile();
+                    try (BufferedReader reader = new BufferedReader(new FileReader(currentFile))) {
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line).append("\n");
+                        }
+                        textArea.setText(sb.toString());
+                        JOptionPane.showMessageDialog(frame, "File opened successfully", "Open Status",
+                                JOptionPane.INFORMATION_MESSAGE);
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(frame, "Error opening file: " + ex.getMessage(), "Error",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+
+                }
+            }
+        });
+
         // saveButton
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                int choice = fileChooser.showSaveDialog(frame);
 
-            }
-        });
+                if (choice == JFileChooser.APPROVE_OPTION) {
+                    File currentFile = fileChooser.getSelectedFile();
 
-        // openButton
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+                    try (PrintWriter writer = new PrintWriter(new FileWriter(currentFile))) {
+                        writer.println(textArea.getText());
+                        JOptionPane.showMessageDialog(frame, "File saved successfully", "Save Status",
+                                JOptionPane.INFORMATION_MESSAGE);
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(frame, "Error saving file", ex.getMessage(),
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                }
             }
         });
 
